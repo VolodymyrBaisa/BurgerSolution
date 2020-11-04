f66f51a5747a7b29999b5c6a292a4f92989d15b8
Volodymyr Baisa <biosvova@gmail.com>
2020-11-03 22:49:20 -0500

    add server and empty files connection and, indexController

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const expHandlebars = require("express-handlebars");
+const routes = require("./controllers/routes/indexController.js");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+//Static content for the app
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+
+app.engine("handlebars", expHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use();
