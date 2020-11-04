f24f75d276551723fafcfb695ee30d5e08e90511
Volodymyr Baisa <biosvova@gmail.com>
2020-11-03 23:17:32 -0500

    add seeds db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Double Decker Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Triple Decker Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Burger', FALSE);
