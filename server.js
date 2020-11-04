0638a0d3c8bba94e88381be64f44a00dbe552ce4
Volodymyr Baisa <biosvova@gmail.com>
2020-11-03 23:37:02 -0500

    fix server, add route "/", fix images link path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json);
+app.use(express.json());
 
 app.engine("handlebars", expHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
