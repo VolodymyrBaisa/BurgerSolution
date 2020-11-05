a91ef15f5eab680ea21741783acfd4ac21f1b533
Volodymyr Baisa <biosvova@gmail.com>
2020-11-05 02:08:48 -0500

    update gitignore, add MySQL connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "0000",
-    database: "eat_da_burger_db",
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "0000",
+        database: "eat_da_burger_db",
+    });
+}
 
 connection.connect((err) =>
     err
